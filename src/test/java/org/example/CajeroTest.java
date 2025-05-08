8d796ec9469056ea5bb5a7fe686f79586cf917c6
Fulloa08 <f.ulloa08@ufromail.cl>
2025-05-08 00:41:00 -0400

    creación de Test

---
+++ src/test/java/org/example/CajeroTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CajeroTest {
+
+    @Test
+    void retirar() {
+    }
+
+    @Test
+    void depositar() {
+    }
+
+    @Test
+    void obtenerSaldo() {
+    }
+}
